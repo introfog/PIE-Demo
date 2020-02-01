44288f350aabc25e153debd133a3d0aed8a15405
Dmitry Chubrick <dima.chubrick@duallab.com>
2020-02-01 18:54:33 +0300

    #1 Update code style

--- src/main/java/com/introfog/pie/demo/MouseEvents.java
+++ src/main/java/com/introfog/pie/demo/MouseEvents.java
@@ -1,48 +1,55 @@
 package com.introfog.pie.demo;
 
-import com.introfog.pie.core.*;
+import com.introfog.pie.core.Circle;
+import com.introfog.pie.core.Polygon;
+import com.introfog.pie.core.World;
 
-import java.awt.event.*;
-import java.applet.*;
+import java.applet.Applet;
 
-public class MouseEvents extends Applet implements MouseListener{
-	@Override
-	public void init (){
-		addMouseListener (this);
-	}
-	
-	@Override
-	public void mouseClicked (MouseEvent me){ }
-	
-	@Override
-	public void mouseEntered (MouseEvent me){ }
-	
-	@Override
-	public void mouseExited (MouseEvent me){ }
-	
-	@Override
-	public void mousePressed (MouseEvent me){
-		float mouseX = me.getX ();
-		float mouseY = me.getY ();
-		
-		if (me.getButton () == MouseEvent.BUTTON1){
-			Circle circle;
-			float rand = (float) Math.random ();
-			circle = new Circle (rand * 20f + 5f, mouseX, mouseY,  1f, 0.2f);
-			World.getInstance ().addShape (circle);
-		}
-		else if (me.getButton () == MouseEvent.BUTTON3){
-			Polygon rectangle;
-			float rand = (float) Math.random ();
-			float height = rand * 80f + 20f;
-			rand = (float) Math.random ();
-			float width = rand * 80f + 20f;
-			rectangle = Polygon.generateRectangle (mouseX, mouseY, width, height, 1f, 0.2f);
-			rectangle.setOrientation ((float) Math.PI / 6f);
-			World.getInstance ().addShape (rectangle);
-		}
-	}
-	
-	@Override
-	public void mouseReleased (MouseEvent me){ }
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseEvents extends Applet implements MouseListener {
+    @Override
+    public void init() {
+        addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me) {
+        float mouseX = me.getX();
+        float mouseY = me.getY();
+
+        if (me.getButton() == MouseEvent.BUTTON1) {
+            Circle circle;
+            float rand = (float) Math.random();
+            circle = new Circle(rand * 20f + 5f, mouseX, mouseY, 1f, 0.2f);
+            World.getInstance().addShape(circle);
+        } else if (me.getButton() == MouseEvent.BUTTON3) {
+            Polygon rectangle;
+            float rand = (float) Math.random();
+            float height = rand * 80f + 20f;
+            rand = (float) Math.random();
+            float width = rand * 80f + 20f;
+            rectangle = Polygon.generateRectangle(mouseX, mouseY, width, height, 1f, 0.2f);
+            rectangle.setOrientation((float) Math.PI / 6f);
+            World.getInstance().addShape(rectangle);
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+    }
 }
